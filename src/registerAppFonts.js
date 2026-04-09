534752e51d7eebbf3b9cfa41413e8bf415b73568
MoguJunn <MoguJunn@users.noreply.github.com>
2026-04-10 03:34:40 +0800

    feat: 接入多语言字体底座

---
+++ src/registerAppFonts.js
@@ -0,0 +1,6 @@
+import './generated/fonts/judou/regular/result.css'
+import './generated/fonts/judou/medium/result.css'
+import './generated/fonts/judou/bold/result.css'
+import './generated/fonts/judou/heavy/result.css'
+
+export {}
